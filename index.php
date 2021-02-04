24b0361dbfd8e2f988e6ddf1dd03cc547cd93d94
softisoDev <ismayil.developer@gmail.com>
2021-02-04 16:19:34 +0400

    wip

--- index.php
+++ index.php
@@ -4,8 +4,9 @@ use Softiso\PriceParser\Trendyol;
 
 require_once 'vendor/autoload.php';
 
-$url = 'https://www.trendyol.com/bershka/erkek-kum-rengi-kapusonlu-sweatshirt-p-55752533?boutiqueId=551482&merchantId=104961';
-//$url = 'https://www.trendyol.com/bayefendi/kapusonlu-sweatshirt-mood-p-70089784';
+//$url = 'https://www.trendyol.com/bershka/erkek-kum-rengi-kapusonlu-sweatshirt-p-55752533?boutiqueId=551482&merchantId=104961';
+//$url = 'https://www.trendyol.com/izla/sir-agda-makine-2-adet-pudrali-soyulabilen-boncuk-agda-spatula-seti-97854253214521-p-34417675';
+$url = 'https://www.trendyol.com/kumtel/blackthree-3-lu-ankastre-set-b66-s2-firin-ko-410-bf-ocak-da6-833-davlumbaz-p-63038917';
 
 $parser  = new Trendyol();
 echo $parser->url($url)->getPrice();
--- src/Trendyol.php
+++ src/Trendyol.php
@@ -18,8 +18,8 @@ class Trendyol extends BaseParser
         $class = $this->crawler($html)->filter(".prc-slg")->text();
         $js = $this->crawler($html)->filterXPath('//script')->each(function ($node) {
             if (str_contains($node->text(), '__PRODUCT_DETAIL_APP_INITIAL_STATE__')) {
-                preg_match('/=(.*?);/', $node->text(), $out);
-                dd(json_decode(trim($out[1] . '"}}}'), true));
+                preg_match('/=(.*?)};/', utf8_decode($node->text()), $out);
+                dd(json_decode(trim($out[1] . '}'), true));
 
             }
         });
@@ -36,7 +36,7 @@ class Trendyol extends BaseParser
         return [
             'regex' => [
                 [
-                    'pattern' => '/=(.*?);/',
+                    'pattern' => '/=(.*?)};/',
                     'index' => 1,
                     'refine' => function ($content) {
                         return trim($content . '"}}}');
