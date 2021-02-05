40375d26c241fa0371dbe0b426e638f41960e387
softisoDev <ismayil.developer@gmail.com>
2021-02-05 04:09:50 +0400

    defacto

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-use Softiso\PriceParser\Ebay;
+use Softiso\PriceParser\N11;
 
 require_once 'vendor/autoload.php';
 
@@ -8,7 +8,8 @@ require_once 'vendor/autoload.php';
 //$url = 'https://www.trendyol.com/izla/sir-agda-makine-2-adet-pudrali-soyulabilen-boncuk-agda-spatula-seti-97854253214521-p-34417675';
 //$url = 'https://www.trendyol.com/kumtel/blackthree-3-lu-ankastre-set-b66-s2-firin-ko-410-bf-ocak-da6-833-davlumbaz-p-63038917';
 
-$url = 'https://www.gittigidiyor.com/giyim-aksesuar/dexter-marinella-z911-1_pdp_644404522';
-$parser = new Ebay();
+$url = 'https://urun.n11.com/sony-playstation-4/ps4-pes-2021-turkce-ps5-uyumlu-sifir-seritli-ps4-pes-21-P448524514';
+
+$parser = new N11();
 
 dd($parser->url($url)->getPrice());
